dd00a018d57324e4c4cf69a8b5f47fba4d4da657
rosemarie betrong <rmbt.lab@gmail.com>
2020-01-30 20:55:28 -0500

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# puts 'Cleaning database...'
-# Flat.destroy_all
+puts 'Cleaning database...'
+Flat.destroy_all
 
-# puts 'Creating flats...'
-# restaurants_attributes = [
-#   {
-#     name: 'Light & Spacious Garden Flat London',
-#     address: '10 Clifton Gardens London W9 1DT',
-#     description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-#     price_per_night: 75,
-#     number_of_guests: 3
-#   },
-#   {
-#     name: 'Industrial Studio in DUMBO',
-#     address: '20 Avenue of Americas New York 10019',
-#     description: 'blablablalba',
-#     price_per_night: 200,
-#     number_of_guests: 2
-#   }
-# ]
-# Flat.create!(restaurants_attributes)
-# puts 'Finished!'
+puts 'Creating flats...'
+restaurants_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Industrial Studio in the City',
+    address: '23 Diana Road London SW6 6JP',
+    description: 'blablablalba',
+    price_per_night: 200,
+    number_of_guests: 2
+  },
+  {
+    name: 'Stylish House Close to River Thames',
+    address: '5 Queensmill Road London SW6 6JP',
+    description: 'blablablalba',
+    price_per_night: 65,
+    number_of_guests: 2
+  },
+  {
+    name: 'St Pancras Clock Tower Guest Suite',
+    address: 'Euston Rd London N1C 4QP',
+    description: 'blablablalba',
+    price_per_night: 110,
+    number_of_guests: 2
+  }
+]
+Flat.create!(restaurants_attributes)
+puts 'Finished!'
