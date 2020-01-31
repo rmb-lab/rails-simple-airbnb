8dfcb47d8eee4b9d70637fc951f059af5cdd5673
rosemarie betrong <rmbt.lab@gmail.com>
2020-01-30 20:47:34 -0500

    Flat model added + seed draft + routes added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts 'Cleaning database...'
+# Flat.destroy_all
+
+# puts 'Creating flats...'
+# restaurants_attributes = [
+#   {
+#     name: 'Light & Spacious Garden Flat London',
+#     address: '10 Clifton Gardens London W9 1DT',
+#     description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+#     price_per_night: 75,
+#     number_of_guests: 3
+#   },
+#   {
+#     name: 'Industrial Studio in DUMBO',
+#     address: '20 Avenue of Americas New York 10019',
+#     description: 'blablablalba',
+#     price_per_night: 200,
+#     number_of_guests: 2
+#   }
+# ]
+# Flat.create!(restaurants_attributes)
+# puts 'Finished!'
